2c462dc179e4ad02035c66fc6f0dc78e687a77a0
amalho26 <amalho26@uwo.ca>
2021-11-26 15:58:15 -0500

    changed comments of code

--- assignment3/src/main.py
+++ assignment3/src/main.py
@@ -1,25 +1,28 @@
+# this code is just a snippet of the whole insertion program. All of our data was generated using
+# online data generators and put into csv files. From there, we used the pandas and mysql library
+# to transfer the data from the csv files to the database. 
 import mysql
 import pandas as pd
 import mysql.connector as mysql
 from mysql.connector import Error
-empdata = pd.read_csv('/Users/nadinalqarawi/Desktop/evidence.csv', index_col=False, delimiter = ',')
+empdata = pd.read_csv('location_of_file.csv', index_col=False, delimiter = ',')
 empdata.head()
 
-# this try/except is for connecting to db, import data from csv, put into sql table
-try:
+
+try: #try connecting to mysql database
     conn = mysql.connect(host='localhost',database='police',user='root',password='******')
     if conn.is_connected():
         cursor = conn.cursor()
         cursor.execute("select database();")
         record = cursor.fetchone()
 
-        for i, row in empdata.iterrows():
+        for i, row in empdata.iterrows(): #for every entry in csv file, add it to appropriate relation
 
-            sql = "INSERT INTO police.evidence VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            sql = "INSERT INTO police.evidence VALUES (%s, %s, %s, %s, %s, %s, %s)" #sql query
             cursor.execute(sql, tuple(row))
-            print("Record inserted")
+            print("Record inserted") #console confirmation that data is being inserted
 
             conn.commit()
 
-except Error as e:
+except Error as e: #if db connection fails, print out to console and the relevant error
     print("Error while connecting to MySQL", e)
